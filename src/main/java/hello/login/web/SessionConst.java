2cbfdad65250bdbf484b282ca7994a27a5d6e2a6
Dragonspine <ghoul300@naver.com>
2022-09-25 01:04:08 +0900

    feat: login with HttpSession

---
+++ src/main/java/hello/login/web/SessionConst.java
@@ -0,0 +1,24 @@
+package hello.login.web;
+
+/**
+ * packageName    : hello.login.web
+ * fileName       : SessionConst
+ * author         : kanghyun Kim
+ * date           : 2022/09/25
+ * description    :
+ * ===========================================================
+ * DATE              AUTHOR             NOTE
+ * -----------------------------------------------------------
+ * 2022/09/25        kanghyun Kim      최초 생성
+ */
+public interface SessionConst {
+    String LOGIN_MEMBER = "loginMember";
+}
+
+//public abstract class SessionConst {
+//    public static final String LOGIN_MEMBER = "loginMember";
+//}
+
+//public class SessionConst {
+//    public static final String LOGIN_MEMBER = "loginMember";
+//}
